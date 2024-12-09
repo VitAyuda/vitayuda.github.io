a5718c6e169819ec26a4ab41afd7a3016884b123
qgerman2 <qgerman2@gmail.com>
2024-12-09 00:12:27 -0300

    sin argumento base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
     site: "https://carvargas2018.github.io/ifp",
-    base: "ifp",
     integrations: [
         starlight({
             title: "My Docs",
